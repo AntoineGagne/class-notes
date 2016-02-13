51733501787aeedd19513b241d5835821239e402
Antoine Gagné <gagnantoine@gmail.com>
2016-02-12 20:42:11 -0500

    Fixes mobile menu

--- web_notes/scripts/menu.js
+++ web_notes/scripts/menu.js
@@ -4,26 +4,12 @@ var button = document.getElementById("toggle-nav");
 
 button.addEventListener("click", function() {
     var tableOfContents = document.getElementById("TOC");
-    var content = document.getElementsByClassName("level1");
-    var header = document.getElementsByTagName("header")[0];
-    var introduction_text = document.getElementsByTagName("p")[0];
     var navbar = document.getElementsByClassName("navbar")[0];
     if (tableOfContents.className === "") {
-        for (var i = 0; i < content.length; ++i) {
-            content[i].className += "";
-            content[i].className = "level1";
-        }
-        introduction_text.className = "";
-        header.className = "";
         navbar.id = "";
         tableOfContents.className = "mobile-hidden";
     }
     else {
-        for (var i = 0; i < content.length; ++i) {
-            content[i].className += " mobile-hidden";
-        }
-        introduction_text.className = "mobile-hidden";
-        header.className = "mobile-hidden";
         navbar.id = "fixed-navbar";
         tableOfContents.className = "";
     }
