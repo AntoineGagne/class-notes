9fb8c4caab5d235bed1fa621664e1700bb26183f
Antoine Gagné <gagnantoine@gmail.com>
2016-10-18 14:04:49 -0400

    Changes the main page so that it works on mobile

--- templates/scripts/menu.js
+++ templates/scripts/menu.js
@@ -1,16 +1,18 @@
-document.getElementById("TOC").className = "mobile-hidden";
+window.onload = function () {
+    document.getElementById("TOC").className = "mobile-hidden";
 
-var button = document.getElementById("toggle-nav");
+    const button = document.getElementById("toggle-nav");
 
-button.addEventListener("click", function() {
-    var tableOfContents = document.getElementById("TOC");
-    var navbar = document.getElementsByClassName("navbar")[0];
-    if (tableOfContents.className === "") {
-        navbar.id = "";
-        tableOfContents.className = "mobile-hidden";
-    }
-    else {
-        navbar.id = "fixed-navbar";
-        tableOfContents.className = "";
-    }
-}, false);
+    button.addEventListener("click", function() {
+        const tableOfContents = document.getElementById("TOC");
+        const navbar = document.getElementsByClassName("navbar")[0];
+        if (tableOfContents.className === "") {
+            navbar.id = "";
+            tableOfContents.className = "mobile-hidden";
+        }
+        else {
+            navbar.id = "fixed-navbar";
+            tableOfContents.className = "";
+        }
+    }, false);
+}
